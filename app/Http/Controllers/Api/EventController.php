f7ed0f4b64a3ef4e3707cd5b726848bb6ff5fd70
Hariharan <hariharan9572@gmail.com>
2025-08-13 06:52:09 +0530

    event-management

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,32 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        $query = Event::query();
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach($relations as $relation){
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
+        return EventResource::collection(
+            $query->latest()->paginate()
+        );
+    }
+
+    protected function shouldIncludeRelation(string $relation): bool
+    {
+        $include = request()->query('include');
+
+        if(!$include){
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+       return in_array($relation, $relations);
     }
 
     /**
@@ -31,7 +57,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -39,7 +65,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -56,7 +83,7 @@ class EventController extends Controller
         ])
     );
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -66,8 +93,6 @@ class EventController extends Controller
     {
         $event->delete();
 
-        return response()->json([
-           'message'  => "Event deleted successfully"
-        ]);
+        return response(status: 204);
     }
 }
